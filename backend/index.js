c7b152f4f3effc71b9efc96b304c678dc2ff8285
Juan Daniel <jg585480@gmail.com>
2025-04-19 17:23:47 -0500

    update backend

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Redirigir todas las rutas al index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
 // Definir un usuario de prueba
